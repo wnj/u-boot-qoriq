da99e505bce3569806e3ee264c11c8d031474c18
Dmitry Lavnikevich <dmitry.lavnikevich@scalys.by>
2021-01-21 01:24:17 +0300

    Add initial TrustBox XL support
    Based on LS1046FRWY implementation.

---
+++ board/scalys/trustbox-xl/ddr.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2021 Scalys B.V.
+ * opensource@scalys.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <fsl_ddr_sdram.h>
+#include <fsl_ddr_dimm_params.h>
+#ifdef CONFIG_FSL_DEEP_SLEEP
+#include <fsl_sleep.h>
+#endif
+#include <asm/arch/clock.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int fsl_initdram(void)
+{
+	gd->ram_size = tfa_get_dram_size();
+
+	if (!gd->ram_size)
+		gd->ram_size = fsl_ddr_sdram_size();
+
+	return 0;
+}
